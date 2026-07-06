863913dcada33b79a33d81bd595ce735d44e0947
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2026-07-07 06:47:58 +0800

    Refactor SQL queries in modul 3.sql
    Removed unnecessary blank lines and cleaned up SQL queries.

--- modul 3.sql
+++ modul 3.sql
@@ -7,8 +7,6 @@ USE praktikum_abd;
     (praktikum_abd.tr_penjualan.jumlah_barang * praktikum_abd.tr_penjualan.harga_barang) AS total_bayar  
 FROM tr_penjualan;
 
-
-
 SELECT 
     (10 = 10) AS Soal_1,
     (5 != 5) AS Soal_2,
@@ -17,10 +15,6 @@ SELECT
     ('A' = 'a') AS Soal_5,
     (100 > NULL) AS Soal_6;
 
-
--- -------------------------------------------------------------------------
--- Praktek 7: Perbandingan Logika pada Kolom Tabel
--- -------------------------------------------------------------------------
 SELECT 
     kode_transaksi, 
     kode_produk, 
@@ -28,20 +22,8 @@ SELECT
     (jumlah_barang > 3) AS Lebih_Dari_3 
 FROM tr_penjualan;
 
-
--- -------------------------------------------------------------------------
--- Praktek 11: Penggunaan WHERE untuk Filtering Data Spesifik
--- -------------------------------------------------------------------------
 SELECT * FROM tr_penjualan WHERE jumlah_barang = 2;
 
-
--- -------------------------------------------------------------------------
--- Filtering Menggunakan Operator LIKE (Mencari Nama Berakhiran 'a')
--- -------------------------------------------------------------------------
 SELECT * FROM pelanggan WHERE nama_customer LIKE '%a';
 
-
--- -------------------------------------------------------------------------
--- Praktek 15: Filtering Kombinasi dengan Operator Logika AND
--- -------------------------------------------------------------------------
 SELECT * FROM tr_penjualan WHERE jumlah_barang > 1 AND harga_barang > 5000;
